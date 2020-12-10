5fd02b77e69d3c70c5b2ab25a42df7a9aeaeda6b
Phillip Ashe <pashe@calico-jack.local>
2020-12-10 13:34:10 -0500

    successful api call on load

--- src/App.js
+++ src/App.js
@@ -1,9 +1,30 @@
 // import logo from './logo.svg';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
+
+  async function getPost() {
+    let url = 'https://api.randomuser.me';
+    const res = await fetch(url);
+    const data = await res.json();
+    console.log(data);
+    // return data;
+    getPerson(person = data);
+  }
+
   let [posts, addPost] = useState(['abcd']);
+  let [person, getPerson] = useState({});
+  /*
+     need useEffect to prevent infinite calls
+     see:
+     https://medium.com/javascript-in-plain-english/react-hooks-how-to-use-useeffect-ecea3e90d84f
+  */
+
+  useEffect(() => {
+    getPost();
+  }, []);
+
   return (
     <div>
       <header>Tumblr original</header>
@@ -11,6 +32,7 @@ function App() {
         <label for="blogname">Enter name of blog:</label>
         <input type="text" id="blogname" name="blogname" />
         <button onClick={() => addPost(posts = posts.concat(['goodbye world']))}></button>
+        <div>{JSON.stringify(person)}</div>
         <ul>
           {posts.map(post => {
             return <li>{post}</li>;
