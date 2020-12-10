06fd970409b83d29d5bc761b8469edca1adffbed
Phillip Ashe <pashe@calico-jack.local>
2020-12-10 15:50:32 -0500

    console logging original posts

--- src/App.js
+++ src/App.js
@@ -12,17 +12,28 @@ function App() {
     }
     const res = await fetch(url, { headers });
     const data = await res.json();
-    console.log(data);
+    // console.log(data);
+    
+    if (res.status !== 200) {
+      getPerson(person = 'Not found');
+    }
+
     // return data;
-    if (data.response.posts.length) {
+    if (res.status === 200 && data.response.posts.length) {
+      for (let post of data.response.posts) {
+        if (!post.trail.length) {
+          if (post.summary) console.log(`${post.summary} ${post.shortUrl}`);
+        }
+      }
       getPerson(person = data);
       // re-enable to get constant fetching
-      // getPost(blogName, pageNumber + 1);
+      getPost(blogName, pageNumber + 1);
     }
   }
 
-  let [posts, addPost] = useState(['abcd']);
+  // let [posts, addPost] = useState(['abcd']);
   let [person, getPerson] = useState({});
+  let [blogName, setBlogName] = useState('');
   /*
      need useEffect to prevent infinite calls
      see:
@@ -31,7 +42,7 @@ function App() {
 
   useEffect(() => {
     // add blogname here
-    getPost('', 1);
+    // getPost('', 1);
   }, []);
 
   return (
@@ -39,14 +50,19 @@ function App() {
       <header>Tumblr original</header>
       <body>
         <label for="blogname">Enter name of blog:</label>
-        <input type="text" id="blogname" name="blogname" />
-        <button onClick={() => addPost(posts = posts.concat(['goodbye world']))}></button>
+        <input type="text" 
+        id="blogname" 
+        name="blogname" 
+        value={blogName}
+        onChange={e => setBlogName(e.target.value)}
+        />
+        <button onClick={() => getPost(blogName, 1)}></button>
         <div>{JSON.stringify(person)}</div>
-        <ul>
+        {/* <ul>
           {posts.map(post => {
             return <li>{post}</li>;
           })}
-        </ul>
+        </ul> */}
       </body>
     </div>
   );
