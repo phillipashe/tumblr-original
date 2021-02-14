311a67edbc3f168c48c82eeb14137bfb66ef353e
Phillip Ashe <pashe@Phillips-MacBook-Pro.local>
2021-02-14 13:17:43 -0500

    bugfixes, added profile picture

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
-// import logo from './logo.svg';
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 import Post from './components/Post';
 import './App.css';
 
@@ -11,16 +10,19 @@ function App() {
     })
     // apparently the first img in the array is unadulterated
     if (imgObj) return imgObj.media[0].url;
-    return false; 
+    return false;
   }
 
-  async function getPost(blogName, pageNumber) {
+  async function getPost(Name, pageNumber) {
     setError(error = false);
+    setLogin(login = false);
     let url;
     url = `https://api.tumblr.com/v2/blog/${blogName}/posts?offset=${pageNumber * 20}`;
     if (!pageNumber) {
       url = `https://api.tumblr.com/v2/blog/${blogName}/posts`;
       accessOriginalPosts(originalPosts = []);
+      setProfilePic(profPicUrl = "");
+
     }
     const headers = {
       accept: 'application/json;format=camelcase',
@@ -28,15 +30,20 @@ function App() {
     }
     const res = await fetch(url, { headers });
     const data = await res.json();
-    
+
     if (res.status !== 200) {
-      // accessOriginalPosts(originalPosts = 'Not found');
-      setError(error = true);
+      // (data.errors && data.errors[0].code === 4012) ? setLogin(login = true): setError(error = true);
+      (data.errors && data.errors.find(err => err.code === 4012)) ? setLogin(login = true): setError(error = true);
+      // setError(error = true);
       accessOriginalPosts(originalPosts = []);
     }
 
     // process data
     if (res.status === 200 && data.response.posts.length) {
+      if (!pageNumber) {
+        setProfilePic(profPicUrl = data.response.blog.avatar[0].url);
+      }
+
       for (let post of data.response.posts) {
         // no trail === original post
         if (!post.trail.length) {
@@ -46,12 +53,11 @@ function App() {
             shortUrl: post.shortUrl
           }
           if (imgUrl) originalPost.imgUrl = imgUrl;
-            accessOriginalPosts(originalPosts = originalPosts.concat([originalPost]));
+          accessOriginalPosts(originalPosts = originalPosts.concat([originalPost]));
         }
       }
 
       // recursively call to continue getting posts
-      // re-enable to get constant fetching
       getPost(blogName, pageNumber + 1);
     }
   }
@@ -59,60 +65,39 @@ function App() {
   let [originalPosts, accessOriginalPosts] = useState([]);
   let [blogName, setBlogName] = useState('');
   let [error, setError] = useState(false);
-  /*
-     need useEffect to prevent infinite calls
-     see:
-     https://medium.com/javascript-in-plain-english/react-hooks-how-to-use-useeffect-ecea3e90d84f
-  */
+  let [login, setLogin] = useState(false);
+  let [profPicUrl, setProfilePic] = useState('');
 
- function handleKeyPress(event) {
-  if(event.key === 'Enter') getPost(blogName, 0);
-}
-
-  useEffect(() => {
-    // add blogname here
-    // getPost('', 1);
-    // function onKeyDown(e) {
-    //   if (e.key === 'Enter') getPost(blogName, 0);
-    // }
-    // window.addEventListener('keyDown', onKeyDown);
-    // return () => window.removeEventListener('keyDown', onKeyDown);
-  }, []);
+  function handleKeyPress(event) {
+    if (event.key === 'Enter') getPost(blogName, 0);
+  }
 
   return (
-      <body>
+    <body>
       <div>
-      <div class="form__group field" onKeyPress={(e) => handleKeyPress(e)}>
-        <input type="input" class="form__field" placeholder="Blog Name" name="name" id='name' onChange={e => setBlogName(e.target.value)} required />
-        <label for="name" class="form__label">Blog Name</label>
-        <button type="submit" className="blog-search-submit" onClick={() => getPost(blogName, 0)}>Search!</button>
+        <div class="form__group field" onKeyPress={(e) => handleKeyPress(e)}>
+          <input type="input" class="form__field" spellcheck="false" placeholder="Blog Name" name="name" id='name' onChange={e => setBlogName(e.target.value)} required />
+          <label for="name" class="form__label">Blog Name</label>
+          <button type="submit" className="blog-search-submit" onClick={() => getPost(blogName, 0)}><img src="https://img.icons8.com/FFFFFF/search"></img></button>
+        </div>
+        <span><img class="profile-picture" src={profPicUrl}></img></span>
       </div>
-      {/* <header>Tumblr Original Blog Search</header>
-        <div className="blog-search-container">
-          <label className="blog-search-label">Enter name of blog:</label>
-          <input className="blog-search"
-          type="text" 
-          id="blogname" 
-          name="blogname" 
-          value={blogName}
-          onChange={e => setBlogName(e.target.value)}
-          />
-        <button type="submit" className="blog-search-submit" onClick={() => getPost(blogName, 0)}>Search!</button>
-        </div> */}
+      <div class="error">
         {error && <div className="error">Error!  Blog not found!</div>}
+        {login && <div className="error">Sorry!  Blogs requiring a login are not supported.</div>}
         <div className="post-container">
           {
-          originalPosts.length ? (
-          originalPosts.map(post => {
-            return <Post summary={post.summary}
-             shortUrl={post.shortUrl}
-             imgUrl={post.imgUrl} />;
-          })
-          ) : (null)
+            originalPosts.length ? (
+              originalPosts.map(post => {
+                return <Post summary={post.summary}
+                  shortUrl={post.shortUrl}
+                  imgUrl={post.imgUrl} />;
+              })
+            ) : (null)
           }
-          </div>
-    </div>
-      </body>
+        </div>
+      </div>
+    </body>
   );
 }
 
